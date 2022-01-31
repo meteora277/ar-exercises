c671211ba95f883e2b1f919e8b9f989b79d35735
meteora277 <justin.s.diaz@gmail.com>
2022-01-31 18:50:02 -0500

    add validations on instance of Employee

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, :last_name, :hourly_rate, :store_id, presence: true
+  validates :hourly_rate, numericality: {greater_than_or_equal_to: 40}
+  validates :hourly_rate, numericality: {less_than_or_equal_to: 200}
 end
