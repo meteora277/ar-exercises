34ff7967fbc93d9b6b509c803cfefa4566859e69
meteora277 <justin.s.diaz@gmail.com>
2022-01-31 17:53:14 -0500

    create store revenue queries

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,14 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: false, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+has_mens_apparel = Store.where(mens_apparel: true)
+has_mens_apparel.each {|store| puts "#{store.name} - #{store.annual_revenue}"}
+
+
+low_revenue_womens_stores = Store.where(womens_apparel: true).where("annual_revenue < ?", 1000000)
+low_revenue_womens_stores.each {|store| puts "#{store.name} - #{store.annual_revenue}"}
 # Your code goes here ...
