2639f4ef8321fab5683159495771450f801b1e71
meteora277 <justin.s.diaz@gmail.com>
2022-01-31 18:08:11 -0500

    create revenue queries

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
+total_revenue = Store.sum(:annual_revenue)
+average_revenue = total_revenue / Store.count
+million_dollar_stores = Store.where("annual_revenue > ?", 1000000).count
+puts "total revenue -#{total_revenue}"
+puts "average revenue - #{average_revenue}"
+puts "there are #{million_dollar_stores} stores making over $1000000"
+
 # Your code goes here ...
